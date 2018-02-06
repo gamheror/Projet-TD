40e484993194eafdf63d34a3fcf8afee36cdd81c
gamheror <36196058+gamheror@users.noreply.github.com>
2018-02-06 16:17:16 +0100

    debut menu

---
+++ matrices_jeux.c
@@ -0,0 +1,46 @@
+#include <string.h>
+#include <stdlib.h>
+
+#define N 5
+
+
+
+
+
+char menu(){
+	
+	
+	printf("toutes les possibilitées");
+	
+	int choix;
+	scanf("%i",&choix) 
+		switch(int choix){
+			case (1)
+				//fonction positionnement tour dans matrice
+			case (2)
+			case (3)
+			case (4)
+			
+			
+		}
+	
+	
+	
+
+
+
+}
+
+void main()
+{
+		string map[N][N];
+		
+	
+	
+	
+	
+]
+
+
+
+
